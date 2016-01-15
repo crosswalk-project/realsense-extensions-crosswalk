9348ec526f982fe152e5131ead36eb4d9822c029
Ningxin Hu <ningxin.hu@intel.com>
2016-01-15 10:48:48 +0800

    [DEP][Sample] Remove photo capture in effects DEP samples
    Fix #406

--- sample/motion_effect.js
+++ sample/motion_effect.js
@@ -1,14 +1,9 @@
 var statusElement = document.getElementById('status');
-var startButton = document.getElementById('start');
-var stopButton = document.getElementById('stop');
-var takePhotoButton = document.getElementById('takePhoto');
 var loadPhoto = document.getElementById('loadPhoto');
-
-var previewCanvas = document.getElementById('preview');
 var imageCanvas = document.getElementById('image');
 
 var motionEffect, photoCapture, XDMUtils;
-var previewContext, previewData, imageContext, imageData;
+var imageContext, imageData;
 
 var width = 1920, height = 1080;
 var yaw = 0.0, pitch = 0.0, roll = 0.0, zoom = 0.0;
@@ -68,69 +63,9 @@ function doMothionEffect() {
 }
 
 function main() {
-  photoCapture = realsense.DepthEnabledPhotography.PhotoCapture;
   XDMUtils = realsense.DepthEnabledPhotography.XDMUtils;
 
-  previewContext = previewCanvas.getContext('2d');
   imageContext = imageCanvas.getContext('2d');
-  previewData = previewContext.createImageData(width, height);
-
-  var gettingImage = false;
-  photoCapture.onpreview = function(e) {
-    if (gettingImage)
-      return;
-    gettingImage = true;
-    photoCapture.getPreviewImage().then(
-        function(image) {
-          previewData.data.set(image.data);
-          previewContext.putImageData(previewData, 0, 0);
-          gettingImage = false;
-        }, function() { });
-  };
-
-  photoCapture.onerror = function(e) {
-    statusElement.innerHTML = 'Status Info : onerror: ' + e.status;
-  };
-
-  startButton.onclick = function(e) {
-    statusElement.innerHTML = 'Status Info : Start: ';
-    gettingImage = false;
-    photoCapture.startPreview({
-      colorWidth: 1920,
-      colorHeight: 1080,
-      depthWidth: 480,
-      depthHeight: 360,
-      framerate: 30}).then(
-        function(e) { statusElement.innerHTML += e; },
-        function(e) { statusElement.innerHTML += e; });
-  };
-
-  takePhotoButton.onclick = function(e) {
-    photoCapture.takePhoto().then(
-        function(photo) {
-          photo.queryContainerImage().then(
-              function(image) {
-                imageData =
-                    imageContext.createImageData(image.width, image.height);
-                statusElement.innerHTML = 'Take photo sucessfully';
-                imageData.data.set(image.data);
-                imageContext.putImageData(imageData, 0, 0);
-                hasImage = true;
-
-                if (!motionEffect) {
-                  motionEffect = new realsense.DepthEnabledPhotography.MotionEffect();
-                }
-                motionEffect.init(photo).then(
-                    function() {
-                      isInitialized = true;
-                      doMothionEffect();
-                    },
-                    function(e) { statusElement.innerHTML = e });
-              },
-              function(e) { statusElement.innerHTML = e; });
-        },
-        function(e) { statusElement.innerHTML = e; });
-  };
 
   loadPhoto.addEventListener('change', function(e) {
     var file = loadPhoto.files[0];
@@ -167,10 +102,4 @@ function main() {
         },
         function(e) { statusElement.innerHTML = e; });
   });
-
-  stopButton.onclick = function(e) {
-    statusElement.innerHTML = 'Status Info : Stop: ';
-    photoCapture.stopPreview().then(function(e) { statusElement.innerHTML += e; },
-                                    function(e) { statusElement.innerHTML += e; });
-  };
 }
