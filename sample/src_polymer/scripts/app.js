b5133ced04ea3078bcc60dcb389b50684e1560fb
Leon.Han <leon.han@intel.com>
2016-05-05 13:34:47 +0800

    [Polymer] Enable switching sample pages successfully.
    Every sample page will get 'activated' property change when switching,
    each page should observe 'activated' property change:
      If 'activated' false, MUST release camera access immediately;
      If 'activated' true, can require camera access as what you did before.
    
    BUG=XWALK-6867

--- sample/src_polymer/scripts/app.js
+++ sample/src_polymer/scripts/app.js
@@ -3,11 +3,19 @@
 
   var app = document.querySelector('#app');
 
-  app.selectedPage = 'Scene Perception';
+  app.selectedPage = 'Face';
 
   app._onPageSelected = function(e) {
     console.log('item selected: ' + app.selectedPage);
     app.$.paperDrawerPanel.closeDrawer();
+  };
+
+  app._onPageDeactivated = function(e) {
+    e.detail.item.activated = false;
+  };
+
+  app._onPageActivated = function(e) {
+    e.detail.item.activated = true;
   };
 
   app.addEventListener('dom-change', function() {
